ec1e6a3f7e0a06e9909c2703e21d01470d3b536f
Steven Z <stevencsu@163.com>
2019-03-18 20:43:13 +0800

    update wtf-form

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -7,7 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECURE_KEY'] = 'hard to guess string'
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -16,13 +16,19 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+
+# @app.route('/user/<name>')
+# def user(name):
+#     return render_template('user.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
