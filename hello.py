22680266bfe810d1cd031e89267e6d6b589c1ec0
Steven Z <stevencsu@163.com>
2019-03-20 18:38:39 +0800

    send async email

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -34,12 +35,20 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 
+# 异步发送电子邮件
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+# 自动发邮件
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['FLASK_MAIL_SUBJECT_PREFIX'] + subject, 
     sender=app.config['FLASK_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 class Role(db.Model):
     __tablename__ = 'roles'
